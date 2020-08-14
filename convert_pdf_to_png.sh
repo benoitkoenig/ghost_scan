3523a1252d81c3b467afadefd5c964a5cf22cc24
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-14 12:48:21 +0200

    Dont remove pdf after converting them to png

--- convert_pdf_to_png.sh
+++ convert_pdf_to_png.sh
@@ -3,5 +3,4 @@ ls ./data/pdf/*.pdf | cut -d "/" -f 4 | xargs -L1 -I {} pdftk "./data/pdf/{}" bu
 ls ./tmp/*.pdf | xargs -L1 -I {} inkscape {} -z --export-dpi=300 --export-area-drawing --export-png="{}.png"
 ls ./tmp/*.png | xargs -L1 -I {} convert {} -rotate 90 {}
 mv ./tmp/*.png ./data/png/
-rm ./data/pdf/*.pdf
 rm -r ./tmp
