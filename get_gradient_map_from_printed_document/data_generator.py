c5bff1c68ee11e37c23074166b72a84c1e85f8e2
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-15 10:34:26 +0200

    Re-sizing is now down by first downsizing appropriately if necessary, then adding padding

--- get_gradient_map_from_printed_document/data_generator.py
+++ get_gradient_map_from_printed_document/data_generator.py
@@ -7,12 +7,31 @@ import tensorflow as tf
 from .constants import h, w
 from .model import preprocess_input
 
+def resize(inputTensor):
+  [_, inputH, inputW, _] = inputTensor.shape
+  if (inputH > h) | (inputW > w):
+    factor = min(h / inputH, w / inputW)
+    newH = int(inputH * factor)
+    newW = int(inputW * factor)
+    downsizedTensor = tf.image.resize(inputTensor, (newH, newW))
+  else:
+    downsizedTensor = inputTensor
+
+  [_, downsizedH, downsizedW, _] = downsizedTensor.shape
+  paddingHBefore = (h - downsizedH) // 2
+  paddingHAfter = math.ceil((h - downsizedH) / 2)
+  paddingWBefore = (w - downsizedW) // 2
+  paddingWAfter = math.ceil((w - downsizedW) / 2)
+  resizedTensor = tf.pad(downsizedTensor, [(0, 0), (paddingHBefore, paddingHAfter), (paddingWBefore, paddingWAfter), (0, 0)])
+
+  return resizedTensor
+
 def getTensorFromFilepathPng(filepath):
   imgRGBA = PIL.Image.open(filepath)
   imgRGB = imgRGBA.convert('RGB')
   data = np.asarray(imgRGB) / 255
   tensor = tf.convert_to_tensor([data])
-  resizedTensor = tf.image.resize(tensor, (h, w)) # TODO: adding padding would probably improve the result, though downsizing helps with performance, allowing for faster iterations
+  resizedTensor = resize(tensor)
   return resizedTensor
 
 def getDataGenerator():
