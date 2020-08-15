2a326f8287ef1bfb6dfd362c5a5c82ad7d4a4bad
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-15 13:46:24 +0200

    Fix and update get_gradient_map_from_printed_document/visualize_prediction

--- get_gradient_map_from_printed_document/visualize_prediction.py
+++ get_gradient_map_from_printed_document/visualize_prediction.py
@@ -13,12 +13,30 @@ model.load_weights('weights/my_model.h5')
 X = getTensorFromFilepathPng('./data/printed_document/%s' % filename)
 preprocessedX = preprocess_input(X)
 groundTruth = getTensorFromFilepathPng('./data/printed_gradient_map/%s' % filename)
-prediction = model.predict(preprocessedX, steps=1)
+rawPrediction = model.predict(preprocessedX, steps=1)
 
-print(loss(prediction, groundTruth))
+print('Loss: %s' % loss(rawPrediction, groundTruth).numpy())
 
-fig, axs = plt.subplots(1, 3, figsize=(15, 8))
-axs[0].imshow(preprocessedX.numpy()[0])
-axs[1].imshow((prediction)[0])
-axs[2].imshow(groundTruth.numpy()[0])
+prediction = np.clip(rawPrediction[0], 0, 1)
+
+fig, axs = plt.subplots(2, 3, figsize=(50, 50))
+axs[0, 0].imshow(preprocessedX.numpy()[0])
+axs[0, 1].imshow(prediction)
+axs[0, 2].imshow(groundTruth.numpy()[0])
+
+predictionRed = np.copy(prediction)
+predictionRed[:, :, 1] = 0
+predictionRed[:, :, 2] = 0
+
+predictionGreen = np.copy(prediction)
+predictionGreen[:, :, 0] = 0
+predictionGreen[:, :, 2] = 0
+
+predictionBlue = np.copy(prediction)
+predictionBlue[:, :, 0] = 0
+predictionBlue[:, :, 1] = 0
+
+axs[1, 0].imshow(predictionRed)
+axs[1, 1].imshow(predictionGreen)
+axs[1, 2].imshow(predictionBlue)
 plt.show()
