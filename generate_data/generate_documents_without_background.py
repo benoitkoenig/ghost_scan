5dd071f9249113c6815d88ccc8f9902609126d0f
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-17 13:35:07 +0200

    Generate ground truth for transparent image

---
+++ generate_data/generate_documents_without_background.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+from PIL import Image
+import png
+
+dirpath = os.path.dirname(os.path.realpath(__file__))
+
+for filename in os.listdir('%s/../data/printed_document' % dirpath):
+  if filename[-4:] != '.png':
+    continue
+  rawImg = Image.open('%s/../data/printed_document/%s' % (dirpath, filename))
+  rawGrad = Image.open('%s/../data/printed_gradient_map/%s' % (dirpath, filename))
+  img = np.asarray(rawImg)
+  grad = np.asarray(rawGrad)
+  [redPixel, _, _, _] = np.dsplit(grad, 4)
+  mask = (redPixel == 255)
+  rgbaMask = np.concatenate([mask, mask, mask, mask], axis=2)
+  newImgUnreshaped = rgbaMask * img
+  [height, width, _] = newImgUnreshaped.shape
+  newImg = newImgUnreshaped.reshape((height, width * 4))
+  with open('%s/../data/printed_document_with_transparent_background/%s.png' % (dirpath, filename), 'wb') as f:
+    w = png.Writer(width, height, greyscale=False, alpha=True)
+    w.write(f, newImg)
+  print(filename)
