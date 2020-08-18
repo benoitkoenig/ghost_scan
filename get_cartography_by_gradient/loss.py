0aa41e14d6382511765365152f3db60b50ec111d
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-18 15:43:28 +0200

    Update loss in get_cartography_by_gradient

--- get_cartography_by_gradient/loss.py
+++ get_cartography_by_gradient/loss.py
@@ -3,4 +3,4 @@ import tensorflow as tf
 def loss(gt, pr):
   [tensorRed, _, _] = tf.split(gt, 3, axis=3)
   tensorRelevant = tf.cast(tensorRed == 1, gt.dtype)
-  return tf.math.reduce_mean((tensorRelevant * (gt - pr)) ** 2)
+  return tf.math.reduce_sum((tensorRelevant * (gt - pr)) ** 2) / tf.math.reduce_sum(tensorRelevant)
