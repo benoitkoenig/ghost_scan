Fix the X rotation error

get_a4/train.py
@@ -8,5 +8,5 @@ model = getModel()
 
 loggerCb = tf.keras.callbacks.CSVLogger('./logs/get_a4.csv')
 
-model.fit(gen, steps_per_epoch=10, epochs=3, callbacks=[loggerCb])
+model.fit(gen, steps_per_epoch=10, epochs=40, callbacks=[loggerCb])
 model.save_weights('./weights/get_a4', overwrite=True)

get_a4/visualize_prediction.py
@@ -9,12 +9,14 @@ filename = sys.argv[1]
 
 model = getModel()
 model.load_weights('./weights/get_a4')
-X, Y = getSingleEntry(filename)
+X, Y, rawX = getSingleEntry(filename)
 pred = model.predict(X, steps=1)
 
 print('Loss: %s' % tf.keras.losses.MeanSquaredError()(Y, pred).numpy())
 
-fig, ax = plt.subplots(1, 2, figsize=(50, 50))
-ax[0].imshow(Y.numpy()[0])
-ax[1].imshow(pred[0])
+fig, ax = plt.subplots(1, 4, figsize=(50, 50))
+ax[0].imshow(rawX.numpy()[0])
+ax[1].imshow(X.numpy()[0, :, :, 16:20])
+ax[2].imshow(pred[0])
+ax[3].imshow(Y.numpy()[0])
 plt.show()