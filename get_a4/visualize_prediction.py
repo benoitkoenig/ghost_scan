29a6cde4afb111044b3947e0c7673dda12f75828
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-19 16:15:20 +0200

    Clip predicted pixels to [0, 1] in get_a4 visualization

--- get_a4/visualize_prediction.py
+++ get_a4/visualize_prediction.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 import tensorflow as tf
 
@@ -18,5 +19,6 @@ fig, ax = plt.subplots(1, 4, figsize=(50, 50))
 ax[0].imshow(rawX.numpy()[0])
 ax[1].imshow(X.numpy()[0, :, :, 16:20])
 ax[2].imshow(pred[0])
+ax[2].imshow(np.clip(pred[0], 0, 1))
 ax[3].imshow(Y.numpy()[0])
 plt.show()
