02ab3fbae18e2c12732c4dd0ddcf246c0ea49b99
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-21 15:13:49 +0200

    Update the code, but the problem persists

--- get_carto_by_class/loss.py
+++ get_carto_by_class/loss.py
@@ -1,25 +1,21 @@
 import tensorflow as tf
 
 from ghost_scan.constants import numberOfPoints
+from .constants import h, w
+
+def getGtForHighestPrediction(gt, pr):
+  flattenPixelPreds = tf.reshape(pr, (-1, numberOfPoints))
+  flattenPixelsGt = tf.reshape(gt, (-1, numberOfPoints))
+
+  indexOfEachHighestPrediction = tf.math.argmax(flattenPixelPreds, 0) # 1D tensor of len numberOfPoints
+  allGtChannelsOfEachHighestPrediction = tf.gather(flattenPixelsGt, indexOfEachHighestPrediction)
+  gtForHighestPrediction = tf.linalg.diag_part(allGtChannelsOfEachHighestPrediction)
+  return gtForHighestPrediction
 
 def loss(gt, pr):
-  # gt and pr are both of shape (1, h, w, numberOfPoints)
-
-  # First, get the mask. It is where all gaussian distances are zeros
   mask = tf.cast(gt != 0, dtype=pr.dtype)
-  maskedPreds = mask * pr
-
-  # Then, get the gt of all best predictions
-  flattenPixelPreds = tf.reshape(maskedPreds, (-1, numberOfPoints))
-  flattenPixelsGt = tf.reshape(gt, (-1, numberOfPoints))
-  bestPredPerPoint = tf.math.argmax(flattenPixelPreds, 0) # 1D tensor of len numberOfPoints
-  gtOfBestPrediction = tf.linalg.diag_part(tf.gather(flattenPixelsGt, bestPredPerPoint))
-
-  # The adjusted ground truth is the gt minus the gt of the best prediction. So adjustedGT > 0 for points closer than the current best guess, < 0 otherwise
-  adjustedGt = tf.stop_gradient(gt - gtOfBestPrediction)
-
-  # The loss is the cross-entropy of the prediction times the adjusted ground truth. It will move up where adjustedGT > 0, and down where adjustedGt < 0
+  gtForHighestPrediction = getGtForHighestPrediction(gt, mask * pr) # shape=(numberOfPoints)
+  adjustedGt = tf.stop_gradient(gt - gtForHighestPrediction)
   loss = - mask * adjustedGt * tf.math.log(pr)
-  loss = tf.math.reduce_sum(loss) / tf.math.reduce_sum(maskedPreds)
-
-  return loss
+  meanLoss = tf.math.reduce_sum(loss) / tf.math.reduce_sum(mask)
+  return meanLoss
--- get_carto_by_class/metrics.py
+++ get_carto_by_class/metrics.py
@@ -1,22 +1,20 @@
 import tensorflow as tf
 
 from ghost_scan.constants import numberOfPoints
+from .loss import getGtForHighestPrediction
 
 def meanSquareError(gt, pr):
   # Note: The two first parts are copy/paste from loss.py. I keep them seperated for now
 
   # First, get the mask. It is where all gaussian distances are zeros
   mask = tf.cast(gt != 0, dtype=pr.dtype)
-  maskedPreds = mask * pr
 
   # Then, get the gt of all best predictions
-  flattenPixelPreds = tf.reshape(maskedPreds, (-1, numberOfPoints))
-  flattenPixelsGt = tf.reshape(gt, (-1, numberOfPoints))
-  bestPredPerPoint = tf.math.argmax(flattenPixelPreds, 0) # 1D tensor of len numberOfPoints
-  gtOfBestPrediction = tf.linalg.diag_part(tf.gather(flattenPixelsGt, bestPredPerPoint))
+  gtForHighestPrediction = getGtForHighestPrediction(gt, mask * pr)
+  assert(gtForHighestPrediction.shape == (numberOfPoints))
   
   # Finally, get the meanSquareDistance
-  squareDistances = - tf.math.log(gtOfBestPrediction)
+  squareDistances = - tf.math.log(gtForHighestPrediction)
   meanSquareDistance = tf.math.reduce_mean(squareDistances)
 
   return meanSquareDistance
