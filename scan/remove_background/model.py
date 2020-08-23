74f3eaf0a30c6bb0f4a35630926de62b811eb0a7
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-23 21:59:55 +0200

    Use a custom loss for remove_background (technically just the weighted bce)

--- scan/remove_background/model.py
+++ scan/remove_background/model.py
@@ -2,11 +2,12 @@ import segmentation_models as sm
 import tensorflow as tf
 
 from .constants import h, w
+from .loss import loss
 
 def getModel(weights=None):
   model = sm.Unet('resnet34', input_shape=(h, w, 3), classes=1, activation='sigmoid', weights=None, encoder_weights=None)
   optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
-  model.compile(optimizer=optimizer, loss=tf.keras.losses.BinaryCrossentropy())
+  model.compile(optimizer=optimizer, loss=loss)
   if (weights != None):
     model.load_weights(weights)
   return model
