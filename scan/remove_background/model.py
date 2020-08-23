0c42186a3742baaa5e4cb38e54dc53d4e965b11c
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-23 23:03:17 +0200

    Update remove_background training. Batchsize is still 1, for now

--- scan/remove_background/model.py
+++ scan/remove_background/model.py
@@ -7,7 +7,8 @@ from .loss import loss
 def getModel(weights=None):
   model = sm.Unet('resnet34', input_shape=(h, w, 3), classes=1, activation='sigmoid', weights=None, encoder_weights=None)
   optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
-  model.compile(optimizer=optimizer, loss=loss)
+  IoUMetric = tf.keras.metrics.MeanIoU(num_classes=2)
+  model.compile(optimizer=optimizer, loss=loss, metrics=[IoUMetric])
   if (weights != None):
     model.load_weights(weights)
   return model
--- scan/remove_background/train.py
+++ scan/remove_background/train.py
@@ -7,9 +7,9 @@ from .data_generator import getDataGenerator
 gen = getDataGenerator()
 model = getModel(weights=None)
 
-reduceLrCb = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=4, min_lr=4e-6)
+reduceLrCb = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, min_lr=4e-6)
 loggerCb = tf.keras.callbacks.CSVLogger('./scan/logs/remove_background.csv')
 
-steps_per_epoch = 10
+steps_per_epoch = 20
 model.fit(gen, steps_per_epoch=steps_per_epoch, epochs=filesCount // steps_per_epoch, callbacks=[reduceLrCb, loggerCb])
 model.save_weights('./scan/weights/remove_background/weights', overwrite=True)
