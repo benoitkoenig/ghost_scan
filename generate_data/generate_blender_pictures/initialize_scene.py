b90e54329482c6174f859566b1a21a9f3aedcba8
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-23 15:43:30 +0200

    Add random lightning

--- generate_data/generate_blender_pictures/initialize_scene.py
+++ generate_data/generate_blender_pictures/initialize_scene.py
@@ -19,6 +19,15 @@ def move_camera():
   scene.camera.location = mathutils.Vector([centered_random(), centered_random(), 10 + centered_random()])
   scene.camera.rotation_euler = mathutils.Vector([math.pi * centered_random() / 36, math.pi * centered_random() / 36, math.pi * (centered_random() / 36 - 0.5)])
 
+def set_random_lightning():
+  scene = bpy.context.scene
+  bpy.data.lamps[0].color = [1 - 0.5 * random.random() for _ in range(3)]
+  bpy.data.lamps[0].type = random.choice(['POINT', 'SUN', 'SPOT', 'HEMI', 'AREA'])
+  bpy.data.lamps[0].distance = 20
+  lamp = [o for o in scene.objects if o.type == 'LAMP'][0]
+  lamp.location = mathutils.Vector([10 * centered_random(), 10 * centered_random(), 18 + 5 * centered_random()])
+  lamp.rotation_euler = mathutils.Vector([math.pi * centered_random() / 36, math.pi * centered_random() / 36, math.pi * (centered_random() / 36 - 0.5)])
+
 def set_render_params():
   scene = bpy.data.scenes[0]
   scene.render.image_settings.color_depth = '16'
@@ -29,4 +38,5 @@ def set_render_params():
 def initialize_scene():
   delete_default_cube()
   move_camera()
+  set_random_lightning()
   set_render_params()
