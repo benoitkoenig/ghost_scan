3e8dc02390d2e524d4d7b48ca444fe03d2ac5aab
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-25 13:26:08 +0200

    Remove background's emit

--- generate_data/generate_blender_pictures/prepare_gradient_picture.py
+++ generate_data/generate_blender_pictures/prepare_gradient_picture.py
@@ -11,10 +11,15 @@ def set_lighting_conditions_for_gradient_picture():
   world.horizon_color = 0, 0, 0
   world.zenith_color = 0, 0, 0
 
-  mat = bpy.data.materials.get('MatDocument')
-  mat.emit = 1
-  mat.ambient = 0
-  mat.translucency = 0
+  matDocument = bpy.data.materials.get('MatDocument')
+  matDocument.emit = 1
+  matDocument.ambient = 0
+  matDocument.translucency = 0
+
+  matBackground = bpy.data.materials.get('MatBackground')
+  matBackground.emit = 0
+  matBackground.ambient = 0
+  matBackground.translucency = 0
 
 def prepare_gradient_picture():
   set_lighting_conditions_for_gradient_picture()
