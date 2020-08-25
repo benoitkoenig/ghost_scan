076e9feca8a52a1c7b84a1a014f220370515d17a
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-25 18:52:36 +0200

    Removed get_a4_with_fine_tuning/ and get_carto_by_points, which are obsolete. get_carto_by_gradient remains, as it may still be relevant

--- scan/get_a4_with_fine_tuning/constants.py
+++
@@ -1,3 +0,0 @@
-h = 165
-w = 127
-inputChannels = 9 * 4 + 2
--- scan/get_a4_with_fine_tuning/predict.py
+++
@@ -1,8 +0,0 @@
-from .model import getModel
-from .preprocess import preprocess
-
-def predict(inputTensor, positions):
-  X = preprocess(inputTensor, positions)
-  model = getModel(weights='./scan/weights/get_a4/weights')
-  pred = model.predict(X, steps=1)
-  return pred
