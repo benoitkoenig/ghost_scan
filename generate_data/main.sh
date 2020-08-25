2711eb6c2d235fb2e76a6c992567f71d1e1f39ac
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-25 16:30:52 +0200

    Rename generate_blender_pictures into blender

--- generate_data/main.sh
+++ generate_data/main.sh
@@ -3,7 +3,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $dir/generate_a4_png.sh
 echo '[Done] Generating A4-format png files'
 
-ls $dir/../data/png/*.png | rev | cut -d '/' -f 1 | rev | xargs -L1 -I {} blender -b -P $dir/generate_blender_pictures/main.py -- {}
+ls $dir/../data/png/*.png | rev | cut -d '/' -f 1 | rev | xargs -L1 -I {} blender -b -P $dir/blender/main.py -- {}
 echo '[Done] Generating printed documents and corresponding printed gradient maps'
 
 python3 $dir/generate_documents_without_background.py
