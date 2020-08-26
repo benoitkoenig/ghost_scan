2cde01633f601f7d0bbab7a6ad0c7f3d44ca606f
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-26 11:49:08 +0200

    Does not restart blender between each document, but simply clean the scene

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@ coords = [(0, 0), (0, 1), (1, 0), (1, 1)]
 numberOfPoints = len(coords)
 
 dirpath = os.path.dirname(os.path.realpath(__file__))
-filenames = [f for f in os.listdir('%s/data/printed_gradient_map' % dirpath) if (f[-4:] == '.png')]
+filenames = [f for f in os.listdir('%s/data/png' % dirpath) if (f[-4:] == '.png')]
 filesCount = len(filenames)
--- generate_data/blender/main.py
+++ generate_data/blender/main.py
@@ -1,14 +1,10 @@
-import os
-import sys
-
 from ghost_scan.generate_data.blender.generate_both_pictures import generate_both_pictures
+from ghost_scan.constants import filenames, dirpath
 
-filename = sys.argv[5]
-
-dirname = os.path.dirname(__file__)
-
-generate_both_pictures(
-  inputPath='%s/../../data/png/%s' % (dirname, filename),
-  outputDocumentPath='%s/../../data/printed_document/%s' % (dirname, filename),
-  outputGradientPath='%s/../../data/printed_gradient_map/%s' % (dirname, filename),
-)
+for (index, filename) in enumerate(filenames):
+  generate_both_pictures(
+    inputPath='%s/data/png/%s' % (dirpath, filename),
+    outputDocumentPath='%s/data/printed_document/%s' % (dirpath, filename),
+    outputGradientPath='%s/data/printed_gradient_map/%s' % (dirpath, filename),
+  )
+  print('%s/%s' % (index + 1, len(filenames)))
