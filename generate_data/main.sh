6447d1dbd7a165e8b9964708ebaca5a5d70b044b
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-26 11:55:44 +0200

    Rename blender/main.py into blender/generate_all.py

--- generate_data/main.sh
+++ generate_data/main.sh
@@ -3,7 +3,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $dir/generate_a4_png.sh
 echo '[Done] Generating A4-format png files'
 
-blender -b -P $dir/blender/main.py
+blender -b -P $dir/blender/generate_all.py
 echo '[Done] Generating printed documents and corresponding printed gradient maps'
 
 python3 $dir/generate_documents_without_background.py
