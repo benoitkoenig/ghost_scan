bea80c5b78e2a19a9c2c86e10b26444e0ff977da
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-27 21:46:28 +0200

    Update get_a4. Now its much faster but doesnt work lol

--- scan/get_a4/get_a4.py
+++ scan/get_a4/get_a4.py
@@ -2,24 +2,40 @@ import numpy as np
 
 from .constants import h, w
 
-def calculatePosition(positions, destinationY, destinationX):
-  if (destinationX + destinationY < 1):
-    vectorY = positions[2] - positions[0]
-    vectorX = positions[1] - positions[0]
-    [originY, originX] = positions[0] + (destinationY * vectorY + destinationX * vectorX)
-  else:
-    vectorY = positions[1] - positions[3]
-    vectorX = positions[2] - positions[3]
-    [originY, originX] = positions[3] + ((1 - destinationY) * vectorY + (1 - destinationX) * vectorX)
+def getOrigin(positions):
+  'Returns two numpy arrays, originY and originX, of shape (h, w) that contains, for each pixel in the output, the pixel s coordinates in the input'
+  [gridY, gridX] = np.mgrid[0:h, 0:w]
 
-  return int(round(originY)), int(round(originX))
+  mask1 = (gridY + gridX) < 1
+  mask1 = np.stack([mask1, mask1], axis=-1)
+  origin1 = positions[0]
+  vectorY1 = positions[2] - positions[0]
+  vectorX1 = positions[1] - positions[0]
 
-def getChannels(positions, destinationY, destinationX, inputData):
-  originY, originX = calculatePosition(positions, destinationY, destinationX)
-  pixelData = inputData[originY, originX, 0:3]
-  return pixelData
+  mask2 = 1 - mask1
+  origin2 = positions[3]
+  vectorY2 = positions[1] - positions[3]
+  vectorX2 = positions[2] - positions[3]
+
+  doubleGridY = np.stack([gridY, gridY], axis=-1)
+  doubleGridX = np.stack([gridX, gridX], axis=-1)
+
+  output1 = origin1 + doubleGridY * vectorY1 + doubleGridX * vectorX1
+  output2 = origin2 + (1 - doubleGridY) * vectorY2 + (1 - doubleGridX) * vectorX2
+
+  originPositions = np.round(mask1 * output1 + mask2 * output2)
+  [originY, originX] = np.moveaxis(originPositions, 2, 0)
+  return originY, originX
 
 def getA4(inputData, inputPositions):
   positions = np.array(inputPositions)
-  outputData = [[getChannels(positions, destinationY / h, destinationX / w, inputData) for destinationX in range(w)] for destinationY in range(h)]
-  return outputData
+  originX, originY = getOrigin(positions)
+
+  data = inputData[:, :, 0:3]
+  flatOriginIndices = (originX + data.shape[1] * originY).flatten()
+  flatData = np.reshape(data, (-1, 3))
+
+  flatDestination = np.take(flatData, flatOriginIndices)
+  destination = np.reshape(flatDestination, (h, w, 3))
+
+  return destination
