f38914383d0f48afa081f88d7e70dc583ac34c92
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-27 18:11:02 +0200

    remove_background focuses on borders

---
+++ scan/remove_background/loss.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+epsilon = 1e-7
+
+def loss(gt, pr):
+  blurryGt = tfa.image.median_filter2d(gt, 9, padding='CONSTANT', constant_values=0)
+  weights = (gt * (1 - blurryGt) + (1 - gt) * blurryGt) * 2 + 0.1
+  bce = - (gt * tf.math.log(pr + epsilon) + (1 - gt) * tf.math.log(1 - pr + epsilon))
+  loss = tf.reduce_mean(weights * bce)
+  return loss
--- scan/remove_background/model.py
+++ scan/remove_background/model.py
@@ -2,12 +2,13 @@ import segmentation_models as sm
 import tensorflow as tf
 
 from .constants import h, w
+from .loss import loss
 
 def getModel(weights=None):
   model = sm.Unet('resnet34', input_shape=(h, w, 3), classes=1, activation='sigmoid', weights=None, encoder_weights=None)
-  optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
+  optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
   IoUMetric = sm.metrics.IOUScore(threshold=0.5)
-  model.compile(optimizer=optimizer, loss='bce', metrics=[IoUMetric])
+  model.compile(optimizer=optimizer, loss=loss, metrics=[IoUMetric])
   if (weights != None):
     model.load_weights(weights)
   return model
