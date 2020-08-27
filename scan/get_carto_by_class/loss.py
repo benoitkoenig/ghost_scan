d91888caacffa49a0f4a41b724e92bbf37f707d1
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-27 09:28:04 +0200

    [get_carto_by_class] Update loss function

--- scan/get_carto_by_class/loss.py
+++ scan/get_carto_by_class/loss.py
@@ -1,13 +1,7 @@
 import tensorflow as tf
 
-from ghost_scan.constants import numberOfPoints
-from .constants import h, w
-
 def loss(gt, pr):
   mask = tf.cast(gt != 0, dtype=pr.dtype)
   maskedPr = mask * pr
-  averagePredictedGt = tf.reduce_sum(maskedPr * gt, axis=[0, 1, 2]) / tf.reduce_sum(maskedPr, axis=[0, 1, 2])
-  averagePredictedGt = tf.stop_gradient(averagePredictedGt)
-  adjustedGt = tf.stop_gradient(gt - averagePredictedGt)
-  loss = mask * adjustedGt * (1 - pr) ** 2
-  return loss
+  averages = tf.reduce_sum(maskedPr * gt, axis=[1, 2, 3]) / tf.reduce_sum(maskedPr, axis=[1, 2, 3])
+  return 1 - tf.reduce_mean(averages)
