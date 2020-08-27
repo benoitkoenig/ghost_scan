0cee93627629f5264ee3728fc80e5d6caaac3aa3
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-27 08:52:40 +0200

    Use bce in remove_background

--- scan/remove_background/model.py
+++ scan/remove_background/model.py
@@ -6,9 +6,8 @@ from .constants import h, w
 def getModel(weights=None):
   model = sm.Unet('resnet34', input_shape=(h, w, 3), classes=1, activation='sigmoid', weights=None, encoder_weights=None)
   optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
-  loss = sm.losses.JaccardLoss()
   IoUMetric = sm.metrics.IOUScore(threshold=0.5)
-  model.compile(optimizer=optimizer, loss=loss, metrics=[IoUMetric])
+  model.compile(optimizer=optimizer, loss='bce', metrics=[IoUMetric])
   if (weights != None):
     model.load_weights(weights)
   return model
