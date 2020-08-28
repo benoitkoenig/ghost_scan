d1d6a2f740dc31d74a039b56597a7bc403a405f4
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-29 01:02:23 +0200

    [Blender check] Concatenate both py files. If I could define commands similar to npm scripts, I wouldnt need main.sh either

---
+++ generate_data/blender/check/check.py
@@ -0,0 +1,22 @@
+import os
+
+from ghost_scan.generate_data.blender.generate_both_pictures import generate_both_pictures
+
+dirpath = os.path.dirname(__file__)
+generate_both_pictures(
+  inputPath='%s/input.png' % dirpath,
+  outputDocumentPath='%s/printed_document.png' % dirpath,
+  outputGradientPath='%s/printed_gradient_map.png' % dirpath,
+)
+
+# This file is called in blender context. Importing matplotlib after doing the blender stuff works well - importing it at the beginning of the file results in issues
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+img1 = mpimg.imread('%s/printed_document.png' % dirpath)
+img2 = mpimg.imread('%s/printed_gradient_map.png' % dirpath)
+
+fig, axs = plt.subplots(1, 2, figsize=(50, 50))
+axs[0].imshow(img1)
+axs[1].imshow(img2)
+plt.show()
--- generate_data/blender/check/main.sh
+++ generate_data/blender/check/main.sh
@@ -1,5 +1,2 @@
-set -e
-
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-blender -b -P $dir/generate.py
-python3 $dir/visualize.py
+blender -b -P $dir/check.py
