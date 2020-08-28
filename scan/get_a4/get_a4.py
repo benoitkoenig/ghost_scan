fb579dfffb635cff31c893ab233f6d1879534ce0
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-28 09:09:53 +0200

    [get_a4] Clean code without changing the behavior

--- scan/get_a4/get_a4.py
+++ scan/get_a4/get_a4.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-from .constants import h, w
+from .constants import h as destinationHeight, w as destinationWidth
 
-def getOrigin(positions, h=h, w=w):
-  'Returns two numpy arrays, originY and originX, of shape (h, w) that contains, for each pixel in the output, the pixel s coordinates in the input'
-  [gridY, gridX] = np.mgrid[0:h, 0:w]
-  gridY = gridY.astype(np.float32) / h
-  gridX = gridX.astype(np.float32) / w
+def getOrigin(positions, height=destinationHeight, width=destinationWidth):
+  'Returns origin Y, originX, of shape (destinationHeight, destinationWidth), the coordinates of the pixels in the origin data'
+  [gridY, gridX] = np.mgrid[0:height, 0:width]
+  gridY = gridY / height
+  gridX = gridX / width
+  doubleGridY = np.stack([gridY, gridY], axis=-1)
+  doubleGridX = np.stack([gridX, gridX], axis=-1)
 
-  mask1 = (gridY + gridX) < 1
-  mask1 = np.stack([mask1, mask1], axis=-1)
+  mask1 = (doubleGridY + doubleGridX) < 1
   origin1 = positions[0]
   vectorY1 = positions[2] - positions[0]
   vectorX1 = positions[1] - positions[0]
@@ -19,9 +20,6 @@ def getOrigin(positions, h=h, w=w):
   vectorY2 = positions[1] - positions[3]
   vectorX2 = positions[2] - positions[3]
 
-  doubleGridY = np.stack([gridY, gridY], axis=-1)
-  doubleGridX = np.stack([gridX, gridX], axis=-1)
-
   output1 = origin1 + doubleGridY * vectorY1 + doubleGridX * vectorX1
   output2 = origin2 + (1 - doubleGridY) * vectorY2 + (1 - doubleGridX) * vectorX2
 
@@ -29,18 +27,16 @@ def getOrigin(positions, h=h, w=w):
   [originY, originX] = np.moveaxis(originPositions, 2, 0)
   return originY, originX
 
-def getA4(inputData, inputPositions):
-  positions = np.array(inputPositions, dtype=np.float32)
-  originY, originX = getOrigin(positions)
+def getA4(originData, inputPositions):
+  # In this function, origin is the data from the printed document, destination is the a4-format png
+  originY, originX = getOrigin(np.array(inputPositions))
+  assert (originY.shape == (destinationHeight, destinationWidth)) & (originX.shape == (destinationHeight, destinationWidth))
 
-  data = inputData[:, :, 0:3]
-  flatOriginIndices = (originX + data.shape[1] * originY).flatten()
-  flatData = np.reshape(data, (-1, 3))
+  originWidth = originData.shape[1]
+  flatOriginIndices = (originX + originWidth * originY).flatten()
+  flatOriginData = np.reshape(originData[:, :, 0:3], (-1, 3))
 
-  flatDestinationChannel0 = np.take(flatData[:, 0], flatOriginIndices, mode='wrap')
-  flatDestinationChannel1 = np.take(flatData[:, 1], flatOriginIndices, mode='wrap')
-  flatDestinationChannel2 = np.take(flatData[:, 2], flatOriginIndices, mode='wrap')
-  flatDestination = np.stack([flatDestinationChannel0, flatDestinationChannel1, flatDestinationChannel2], axis=-1)
-  destination = np.reshape(flatDestination, (h, w, 3))
+  flatDestination = np.take(flatOriginData, flatOriginIndices, axis=0, mode='wrap')
+  destinationData = np.reshape(flatDestination, (destinationHeight, destinationWidth, 3))
 
-  return destination
+  return destinationData
