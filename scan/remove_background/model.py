57ec68c8a28c2154516aa6bc476fa926f9bb04fb
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-29 19:23:18 +0200

    Use vgg19 instead of resnet34

--- scan/remove_background/model.py
+++ scan/remove_background/model.py
@@ -5,7 +5,7 @@ from .constants import h, w
 from .loss import loss
 
 def getModel(weights=None):
-  model = sm.Unet('resnet34', input_shape=(h, w, 3), classes=1, activation='sigmoid', weights=None, encoder_weights=None)
+  model = sm.Unet('vgg19', input_shape=(h, w, 3), classes=1, activation='sigmoid', weights=None, encoder_weights=None)
   optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
   IoUMetric = sm.metrics.IOUScore(threshold=0.5)
   model.compile(optimizer=optimizer, loss=loss, metrics=[IoUMetric])
