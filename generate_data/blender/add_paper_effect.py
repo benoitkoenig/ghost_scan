1ead243d6692d704d424e57069f39f3c3c4ff4c9
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-30 19:04:38 +0200

    Add a force field of type force for a slightly curvy effect

--- generate_data/blender/add_paper_effect.py
+++ generate_data/blender/add_paper_effect.py
@@ -24,12 +24,20 @@ def add_softbody_modifier_on_document():
 
 def add_force_fields():
   bpy.ops.object.effector_add(type='WIND')
-  bpy.data.objects['Field'].field.strength = 2.2
+  bpy.data.objects['Field'].field.strength = 1.5
   bpy.ops.object.effector_add(type='TURBULENCE')
   bpy.data.objects['Field.001'].field.strength = 0.5
+  bpy.ops.object.effector_add(type='FORCE')
+  bpy.data.objects['Field.002'].field.strength = random.uniform(0, 3)
+  if (random.random() < 0.5): # Then the force field goes in a corner
+    cornerSign1 = 1 if random.random() < 0.5 else -1
+    cornerSign2 = 1 if random.random() < 0.5 else -1
+    bpy.data.objects['Field.002'].location = [random.uniform(cornerSign1 * 2.5, cornerSign1 * 2), random.uniform(cornerSign2 * 2, cornerSign2 * 1.5), random.uniform(-1, -0.2)]
+  else: # Then the force field goes near the center
+    bpy.data.objects['Field.002'].location = [random.uniform(-2, 2), random.uniform(-1.5, 1.5), random.uniform(-1, -0.2)]
 
 def move_timeframe():
-  for i in range(random.randint(0, 50)):
+  for i in range(random.randint(0, 30)):
     bpy.context.scene.frame_set(i)
 
 def add_paper_effect():
