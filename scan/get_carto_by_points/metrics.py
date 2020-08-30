[get_carto_by_points] Fix errors in loss calculation, update preprocessY, and increase the number of points

scan/get_carto_by_points/metrics.py
@@ -11,7 +11,7 @@ def getGtForHighestPrediction(gt, pr):
   gtForHighestPrediction = tf.linalg.diag_part(allGtChannelsOfEachHighestPrediction)
   return gtForHighestPrediction
 
-def meanSquareError(gt, pr):
+def distance(gt, pr):
   # Note: The two first parts are copy/paste from loss.py. I keep them seperated for now
 
   # First, get the mask. It is where all gaussian distances are zeros
@@ -22,7 +22,7 @@ def meanSquareError(gt, pr):
   assert(gtForHighestPrediction.shape == (numberOfPoints))
 
   # Finally, get the meanSquareDistance
-  squareDistances = - tf.math.log(gtForHighestPrediction)
-  meanSquareDistance = tf.math.reduce_mean(squareDistances)
+  distances = - tf.math.log(gtForHighestPrediction)
+  meanSquareDistance = tf.math.reduce_mean(distances)
 
   return meanSquareDistance

scan/get_carto_by_points/predict.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from ghost_scan.constants import numberOfPoints
 from ghost_scan.scan.resize_with_coords import resizeWithCoords
 from .constants import h, w
 from .model import getModel
@@ -8,7 +9,7 @@ from .postprocess import postprocess
 def predict(inputTensor):
   X, coordsData = resizeWithCoords(inputTensor, h, w)
   mask = (X[:, :, :, 3] == 1).numpy()
-  mask = np.stack([mask, mask, mask, mask], axis=-1)
+  mask = np.repeat(np.expand_dims(mask, axis=-1), numberOfPoints, axis=-1)
   model = getModel(weights='./scan/weights/get_carto_by_points/weights')
   preds = model.predict(X, steps=1)
   maskedPreds = mask * preds

scan/get_carto_by_points/preprocessY.py
@@ -17,7 +17,8 @@ def preprocessY(inputY):
   stackedChannels = tf.repeat(stackedChannels, numberOfPoints, axis=-2)
 
   squareDistances = tf.math.reduce_sum((stackedChannels - pointsGrid) ** 2, axis=-1)
+  distances = tf.math.sqrt(squareDistances)
 
-  Y = tf.math.exp(-squareDistances) # Gaussian of the distance: 1 for points that are close, down to zero for the further points
+  Y = tf.math.exp(-distances) # Gaussian of the rrot square of the distance
   Y = tf.cast(mask, dtype=Y.dtype) * Y
   return Y

scan/get_carto_by_points/visualize_prediction.py
@@ -3,6 +3,7 @@ import sys
 import tensorflow as tf
 import numpy as np
 
+from ghost_scan.constants import numberOfPoints
 from .get_data import getFullData
 from .constants import h, w
 from .loss import loss
@@ -16,7 +17,8 @@ X, groundTruth, rawX, coords = getFullData(filename)
 model = getModel('./scan/weights/get_carto_by_points/weights')
 preds = model.predict(X, steps=1)
 mask = (X.numpy()[:, :, :, 3] == 1)
-maskedPreds = np.stack([mask, mask, mask, mask], axis=-1) * preds
+mask = np.repeat(np.expand_dims(mask, axis=-1), numberOfPoints, axis=-1)
+maskedPreds = mask * preds
 positions = postprocess(maskedPreds, coords, rawX.shape[1:3])
 
 print('Loss: %s' % tf.reduce_mean(loss(groundTruth, preds)).numpy())
@@ -25,9 +27,9 @@ print('MeanSquareError: %s' % meanSquareError(groundTruth, preds).numpy())
 fig, axs = plt.subplots(2, 3, figsize=(50, 50))
 axs[0, 0].imshow(rawX.numpy()[0])
 axs[0, 0].plot(positions[:, 1], positions[:, 0], marker='x')
-axs[1, 0].imshow(groundTruth.numpy()[0, :, :])
-axs[0, 1].imshow(maskedPreds[0, :, :, 0])
-axs[1, 1].imshow(maskedPreds[0, :, :, 1])
-axs[0, 2].imshow(maskedPreds[0, :, :, 2])
-axs[1, 2].imshow(maskedPreds[0, :, :, 3])
+axs[1, 0].imshow(groundTruth.numpy()[0, :, :, 0])
+axs[0, 1].imshow(maskedPreds[0, :, :, 0:3])
+axs[1, 1].imshow(maskedPreds[0, :, :, 3:6])
+axs[0, 2].imshow(maskedPreds[0, :, :, 6:9])
+axs[1, 2].imshow(np.sum(maskedPreds[0], axis=-1))
 plt.show()