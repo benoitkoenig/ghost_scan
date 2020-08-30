445dc0c9b2037bfcb747e00b3d4788fc5e69d0f6
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-30 11:34:33 +0200

    [get_carto_by_points] Loss now has a factorial mean between the loss of each point, compared to a arithmetic mean

--- scan/get_carto_by_points/loss.py
+++ scan/get_carto_by_points/loss.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
 
+from ghost_scan.constants import numberOfPoints
+
 def loss(gt, pr):
   mask = tf.cast(gt != 0, dtype=pr.dtype)
   maskedPr = mask * pr
   averages = tf.reduce_sum(maskedPr * gt, axis=[1, 2, 3]) / tf.reduce_sum(maskedPr, axis=[1, 2, 3])
-  return 1 - tf.reduce_mean(averages)
+  prod_mean = tf.math.pow(tf.reduce_prod(averages), 1 / numberOfPoints)
+  return 1 - prod_mean
