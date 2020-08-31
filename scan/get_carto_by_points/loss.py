6154fb2ea34e22e39fd3e1ca4283371ac55467de
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-31 20:58:22 +0200

    [get_carto_by_points] Add epsilon to denominator in loss

--- scan/get_carto_by_points/loss.py
+++ scan/get_carto_by_points/loss.py
@@ -5,7 +5,7 @@ from ghost_scan.constants import numberOfPoints
 def loss(gt, pr):
   mask = tf.cast(gt != 0, dtype=pr.dtype)
   maskedPr = mask * pr
-  mean_per_channel = tf.reduce_sum(maskedPr * gt, axis=[1, 2]) / tf.reduce_sum(maskedPr, axis=[1, 2])
+  mean_per_channel = tf.reduce_sum(maskedPr * gt, axis=[1, 2]) / (tf.reduce_sum(maskedPr, axis=[1, 2]) + 1e-7)
   mean_per_image = tf.math.pow(tf.reduce_prod(mean_per_channel, axis=1), 1 / numberOfPoints)
   mean_per_batch = tf.math.reduce_mean(mean_per_channel)
   return 1 - mean_per_batch
