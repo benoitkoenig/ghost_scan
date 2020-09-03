85c6a7f9c7924f9e12789ceb336f00d11e04df94
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-03 19:13:48 +0200

    [remove_background] Fix loss function

--- scan/remove_background/loss.py
+++ scan/remove_background/loss.py
@@ -5,5 +5,7 @@ def loss(gt, pr):
   prdx, prdy = tf.image.image_gradients(pr)
   mask = tf.cast((gtdx != 0) | (gtdy != 0), dtype=pr.dtype)
   mse = (gtdx - prdx) ** 2 + (gtdy - prdy) ** 2
-  loss = 0.5 * (tf.reduce_mean(mask * mse) + tf.reduce_mean((1 - mask) * mse))
+  loss1 = tf.math.reduce_sum(mask * mse) / tf.math.reduce_sum(mask)
+  loss0 = tf.math.reduce_sum((1 - mask) * mse) / tf.math.reduce_sum(1 - mask)
+  loss = (loss0 + loss1) / 2
   return loss
