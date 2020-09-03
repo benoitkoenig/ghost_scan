4846fa737ec27937b55e71ed53acaa771bed71cb
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-03 13:07:05 +0200

    [remove_background] Losses averages separately borders and non-borders

--- scan/remove_background/loss.py
+++ scan/remove_background/loss.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 def loss(gt, pr):
   gtdx, gtdy = tf.image.image_gradients(gt)
   prdx, prdy = tf.image.image_gradients(pr)
+  mask = tf.cast((gtdx != 0) | (gtdy != 0), dtype=pr.dtype)
   mse = (gtdx - prdx) ** 2 + (gtdy - prdy) ** 2
-  loss = tf.reduce_mean(mse)
+  loss = 0.5 * (tf.reduce_mean(mask * mse) + tf.reduce_mean((1 - mask) * mse))
   return loss
