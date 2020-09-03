3012faaebf01a41fd286874857347ef26fb2608d
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-03 18:13:29 +0200

    [remove_background] Display loss in visualize_prediction

--- scan/remove_background/visualize_prediction.py
+++ scan/remove_background/visualize_prediction.py
@@ -5,16 +5,19 @@ import numpy as np
 
 from ghost_scan.constants import h, w, dirpath
 from .get_data import getXY
+from .loss import loss
 from .model import getModel
 
 filename = sys.argv[1]
 
 X, groundTruth = getXY([filename])
 model = getModel(weights='%s/scan/weights/remove_background/weights' % dirpath)
-prediction = model.predict(X, steps=1)
+preds = model.predict(X, steps=1)
+
+print('Loss: %s' % loss(groundTruth, tf.convert_to_tensor(preds)).numpy())
 
 fig, axs = plt.subplots(1, 3, figsize=(50, 50))
 axs[0].imshow(X.numpy()[0])
 axs[1].imshow(np.reshape(groundTruth.numpy(), (h, w)))
-axs[2].imshow(np.reshape(prediction, (h, w)))
+axs[2].imshow(np.reshape(preds, (h, w)))
 plt.show()
