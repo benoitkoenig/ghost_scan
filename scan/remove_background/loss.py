823b5b56b9713384363980fac1d5a30554d6dfb1
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-05 14:48:44 +0200

    [remove_background] loss is now a mix between segmentationLoss and gradientLoss

--- scan/remove_background/loss.py
+++ scan/remove_background/loss.py
@@ -1,11 +1,21 @@
 import tensorflow as tf
 
-def loss(gt, pr):
+def gradientLoss(gt, pr):
   gtdx, gtdy = tf.image.image_gradients(gt)
   prdx, prdy = tf.image.image_gradients(pr)
   mask = tf.cast((gtdx != 0) | (gtdy != 0), dtype=pr.dtype)
   mse = (gtdx - prdx) ** 2 + (gtdy - prdy) ** 2
-  loss1 = tf.math.reduce_sum(mask * mse) / tf.math.reduce_sum(mask)
-  loss0 = tf.math.reduce_sum((1 - mask) * mse) / tf.math.reduce_sum(1 - mask)
-  loss = (loss0 + loss1) / 2
+
+  gradientLoss1 = tf.math.reduce_sum(mask * mse) / tf.math.reduce_sum(mask)
+  gradientLoss0 = tf.math.reduce_sum((1 - mask) * mse) / tf.math.reduce_sum(1 - mask)
+  gradientLoss = (gradientLoss0 + gradientLoss1) / 2
+
+  return gradientLoss
+
+def segmentationLoss(gt, pr):
+  segmentationLoss = tf.math.reduce_mean((gt - pr) ** 2)
+  return segmentationLoss
+
+def loss(gt, pr):
+  loss = (gradientLoss(gt, pr) + segmentationLoss(gt, pr)) / 2
   return loss
