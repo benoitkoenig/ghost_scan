d582f5ada9ea0c0b40e2b550f38d10555828fb6e
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-07 10:38:19 +0200

    [get_carto_by_points] Update variable names in bestPredDistance metric

--- scan/get_carto_by_points/metrics.py
+++ scan/get_carto_by_points/metrics.py
@@ -6,23 +6,21 @@ def getGtForHighestPrediction(gt, pr):
   flattenPixelPreds = tf.reshape(pr, (-1, numberOfPoints))
   flattenPixelsGt = tf.reshape(gt, (-1, numberOfPoints))
 
-  indexOfEachHighestPrediction = tf.math.argmax(flattenPixelPreds, 0) # 1D tensor of len numberOfPoints
+  indexOfEachHighestPrediction = tf.math.argmax(flattenPixelPreds, axis=0) # 1D tensor of len numberOfPoints
   allGtChannelsOfEachHighestPrediction = tf.gather(flattenPixelsGt, indexOfEachHighestPrediction)
   gtForHighestPrediction = tf.linalg.diag_part(allGtChannelsOfEachHighestPrediction)
   return gtForHighestPrediction
 
 def bestPredDistance(gt, pr):
-  # Note: The two first parts are copy/paste from loss.py. I keep them seperated for now
-
-  # First, get the mask. It is where all gaussian distances are zeros
+  # First, get the mask. It is where all gt are non-zero
   mask = tf.cast(gt != 0, dtype=pr.dtype)
 
   # Then, get the gt of all best predictions
   gtForHighestPrediction = getGtForHighestPrediction(gt, mask * pr)
   assert(gtForHighestPrediction.shape == (numberOfPoints))
 
-  # Finally, get the meanSquareDistance
+  # Finally, get the meanDistance
   distances = - tf.math.log(gtForHighestPrediction)
-  meanSquareDistance = tf.math.reduce_mean(distances)
+  meanDistance = tf.math.reduce_mean(distances)
 
-  return meanSquareDistance
+  return meanDistance
