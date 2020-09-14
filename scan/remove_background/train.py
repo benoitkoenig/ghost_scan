9155c7e4f78f0f3b39edb638bdaf2da110786dda
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-14 23:16:01 +0200

    [remove_background] remove learning rate scheduler

--- scan/remove_background/train.py
+++ scan/remove_background/train.py
@@ -10,13 +10,12 @@ validationData = getValidationData()
 model = getModel(weights=None)
 
 loggerCb = tf.keras.callbacks.CSVLogger('%s/scan/logs/remove_background.csv' % dirpath)
-lrCb = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-4 if epoch < 200 else 1e-5)
 
 model.fit(
   gen,
   validation_data=validationData,
   epochs=epochs,
   steps_per_epoch=steps_per_epoch,
-  callbacks=[loggerCb, lrCb]
+  callbacks=[loggerCb]
 )
 model.save_weights('%s/scan/weights/remove_background/weights' % dirpath, overwrite=True)
