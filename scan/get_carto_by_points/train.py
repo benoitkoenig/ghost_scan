e8135233784e012568df108a2f74f8891a86019e
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-18 20:33:54 +0200

    [scan] Add learning rate scheduler

--- scan/get_carto_by_points/train.py
+++ scan/get_carto_by_points/train.py
@@ -10,12 +10,13 @@ validationData = getValidationData()
 model = getModel(weights=None)
 
 loggerCb = tf.keras.callbacks.CSVLogger('%s/scan/logs/get_carto_by_points.csv' % dirpath)
+lrSchedulerCb = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 2e-4 if epoch < 350 else 2e-5)
 
 model.fit(
   gen,
   validation_data=validationData,
   epochs=epochs,
   steps_per_epoch=steps_per_epoch,
-  callbacks=[loggerCb]
+  callbacks=[loggerCb, lrSchedulerCb]
 )
 model.save_weights('%s/scan/weights/get_carto_by_points/weights' % dirpath, overwrite=True)
--- scan/remove_background/train.py
+++ scan/remove_background/train.py
@@ -10,12 +10,13 @@ validationData = getValidationData()
 model = getModel(weights=None)
 
 loggerCb = tf.keras.callbacks.CSVLogger('%s/scan/logs/remove_background.csv' % dirpath)
+lrSchedulerCb = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-4 if epoch < 350 else 1e-5)
 
 model.fit(
   gen,
   validation_data=validationData,
   epochs=epochs,
   steps_per_epoch=steps_per_epoch,
-  callbacks=[loggerCb]
+  callbacks=[loggerCb, lrSchedulerCb]
 )
 model.save_weights('%s/scan/weights/remove_background/weights' % dirpath, overwrite=True)
