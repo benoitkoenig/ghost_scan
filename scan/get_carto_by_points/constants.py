f1e107eaef7066f68d43888c27e001d2dfc94d28
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-18 20:17:01 +0200

    [get_carto_by_points] Use the validation dataset

--- scan/get_carto_by_points/constants.py
+++ scan/get_carto_by_points/constants.py
@@ -1,7 +1,6 @@
 from ghost_scan.constants import filesCount
 
-validationSize = 20
 batchSize = 10
 steps_per_epoch = 5
 
-epochs = (filesCount - validationSize) // (steps_per_epoch * batchSize)
+epochs = filesCount // (steps_per_epoch * batchSize)
--- scan/get_carto_by_points/get_data.py
+++ scan/get_carto_by_points/get_data.py
@@ -1,28 +1,25 @@
 from ghost_scan.scan.get_data import getFilenames, loadSingleUnresizedPngTensor, loadPngTensors
 from ghost_scan.scan.resize_with_coords import resizeWithCoords
 from ghost_scan.constants import h, w, dirpath
-from .constants import validationSize, batchSize
+from .constants import batchSize
 from .preprocess import preprocessX, preprocessY
 
-def getXY(filenames):
-  X = loadPngTensors(['%s/data/training/printed_document/%s' % (dirpath, f) for f in filenames], h, w)
+def getXY(filenames, folder='training'):
+  X = loadPngTensors(['%s/data/%s/printed_document/%s' % (dirpath, folder, f) for f in filenames], h, w)
   X = X[:, :, :, 0:3]
   X = preprocessX(X)
-  Y = loadPngTensors(['%s/data/training/printed_gradient_map/%s' % (dirpath, f) for f in filenames], h, w)
+  Y = loadPngTensors(['%s/data/%s/printed_gradient_map/%s' % (dirpath, folder, f) for f in filenames], h, w)
   Y = Y[:, :, :, 0:3]
   Y = preprocessY(Y)
   return X, Y
 
-allFilenames = getFilenames()
-validationSet = allFilenames[:validationSize]
-trainSet = allFilenames[validationSize:]
-
 def getValidationData():
-  X, Y = getXY(validationSet)
+  filenames = getFilenames(folder='validation')
+  X, Y = getXY(filenames, folder='validation')
   return X, Y
 
 def getDataGenerator():
-  filesLeft = [f for f in trainSet]
+  filesLeft = getFilenames()
   while len(filesLeft) != 0:
     batch = filesLeft[:batchSize]
     filesLeft = filesLeft[batchSize:]
