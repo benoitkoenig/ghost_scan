6573ccc0d870c1999e1cbc81f81f1be0965e3cf4
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-18 20:23:51 +0200

    [scan/check] Handle training, validation, real data

--- scan/check.py
+++ scan/check.py
@@ -7,7 +7,12 @@ from ghost_scan.scan.remove_background.predict import RemoveBackground
 from ghost_scan.scan.get_carto_by_points.predict import GetCartoByPoints
 from ghost_scan.scan.get_a4.get_a4 import getA4
 
-isValidationData = ('-r' in sys.argv) | ('--real' in sys.argv)
+if ('-r' in sys.argv) | ('--real' in sys.argv):
+  folderPath = '%s/data/real' % dirpath
+elif ('-v' in sys.argv) | ('--validation' in sys.argv):
+  folderPath = '%s/data/validation' % dirpath
+else:
+  folderPath = '%s/data/training' % dirpath
 
 removeBackground = RemoveBackground()
 getCartoByPoints = GetCartoByPoints()
@@ -16,10 +21,7 @@ while True:
   filename = input('Filename (empty to leave): ')
   if filename == '':
     break
-  if isValidationData:
-    inputImage = loadSingleUnresizedPngTensor('%s/data/real/printed_document/%s' % (dirpath, filename)).numpy()
-  else:
-    inputImage = loadSingleUnresizedPngTensor('%s/data/training/printed_document/%s' % (dirpath, filename)).numpy()
+  inputImage = loadSingleUnresizedPngTensor('%s/printed_document/%s' % (folderPath, filename)).numpy()
   removeBackgroundPreds, coords = removeBackground.predict(inputImage)
   positions = getCartoByPoints.predict(removeBackgroundPreds, coords, inputImage.shape)
   documentA4 = getA4(inputImage[0], positions)
