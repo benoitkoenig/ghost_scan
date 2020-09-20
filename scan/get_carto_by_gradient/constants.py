0a90728c51781d9feabcef765992e9b994684a9d
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-20 13:06:41 +0200

    [scan] Remove get_carto_by_gradients

--- scan/get_carto_by_gradient/constants.py
+++
@@ -1,7 +0,0 @@
-from ghost_scan.constants import filesCount
-
-validationSize = 100
-batchSize = 10
-steps_per_epoch = 5
-
-epochs = (filesCount - validationSize) // (steps_per_epoch * batchSize)
--- scan/get_carto_by_gradient/loss.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def loss(gt, pr):
-  mask = tf.cast(gt[:, :, :, 0] == 1, gt.dtype)
-  mask = tf.stack([mask, mask, mask], axis=-1)
-  loss = tf.math.reduce_sum((mask * (gt - pr)) ** 2) / tf.math.reduce_sum(mask)
-  return loss
