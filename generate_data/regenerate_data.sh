06d1326eade3d0a62e376e8d88373fc1d35f7163
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-20 13:16:38 +0200

    Remove printed_document_without_background, which is now obsolete

--- generate_data/regenerate_data.sh
+++ generate_data/regenerate_data.sh
@@ -3,8 +3,5 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 blender -b -P $dir/blender/generate_all.py -- $1
 echo '[Done] Generating printed documents and corresponding printed gradient maps'
 
-python3 $dir/generate_documents_without_background.py $1
-echo '[Done] Generating documents without background'
-
 python3 $dir/generate_document_carto.py $1
 echo '[Done] Generating documents carto points'
