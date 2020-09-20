f5ceb05ac896fdd543378147c013234b4c99abda
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-20 13:46:12 +0200

    [detect_pose] Remove obsolete line in loss

--- scan/detect_pose/loss.py
+++ scan/detect_pose/loss.py
@@ -4,6 +4,5 @@ from ghost_scan.constants import numberOfPoints
 
 def loss(gt, pr):
   mean_per_channel = tf.reduce_sum(pr * gt, axis=[1, 2]) / (tf.reduce_sum(pr, axis=[1, 2]) + 1e-7) # 1e-7 is useful when the document is fully out of the picture, which can occasionnaly happen
-  mean_per_image = tf.math.pow(tf.reduce_prod(mean_per_channel, axis=1), 1 / numberOfPoints)
   mean_per_batch = tf.math.reduce_mean(mean_per_channel)
   return - mean_per_batch
