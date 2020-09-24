468f98f8357af2f3d381360a284840ac1bf88290
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-24 09:15:07 +0200

    [export_h5_to_json] Add a warning

--- scan/export_h5_to_json.sh
+++ scan/export_h5_to_json.sh
@@ -1,3 +1,9 @@
+read -p "Did you update the h5 weights with export_models_to_h5.py? [y/N] " yn
+case $yn in
+  [Yy] ) ;;
+  * ) exit;;
+esac
+
 tensorflowjs_converter --input_format keras scan/models/h5/remove_background.h5 scan/models/json/remove_background/ --weight_shard_size_bytes 419430400
 tensorflowjs_converter --input_format keras scan/models/h5/detect_pose.h5 scan/models/json/detect_pose/ --weight_shard_size_bytes 419430400
 tensorflowjs_converter --input_format keras scan/models/h5/finetune_positions.h5 scan/models/json/finetune_positions/ --weight_shard_size_bytes 419430400
