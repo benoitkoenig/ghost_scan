f2ce6765e3dd5cf83d3c8c4ab69c10701e14a2df
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-24 14:12:57 +0200

    [scan] Remove obsolete to_json.py files

--- scan/detect_pose/to_json.py
+++
@@ -1,7 +0,0 @@
-import tensorflowjs as tfjs
-
-from ghost_scan.constants import dirpath
-from .model import getModel
-
-model = getModel(weights='%s/scan/models/weights/detect_pose/weights' % dirpath)
-tfjs.converters.save_keras_model(model, '%s/scan/exports_json/detect_pose' % dirpath)
--- scan/finetune_positions/to_json.py
+++
@@ -1,7 +0,0 @@
-import tensorflowjs as tfjs
-
-from ghost_scan.constants import dirpath
-from .model import getModel
-
-model = getModel(weights='%s/scan/models/weights/finetune_positions/weights' % dirpath)
-tfjs.converters.save_keras_model(model, '%s/scan/exports_json/finetune_positions' % dirpath)
