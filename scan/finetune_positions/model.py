0f6035b604347caf762cbac228ea07d4b1e2b9b0
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-30 13:57:54 +0200

    Simplify model

--- scan/finetune_positions/model.py
+++ scan/finetune_positions/model.py
@@ -7,24 +7,10 @@ from .constants import h, w
 def getModel(weights=None):
   model = tf.keras.Sequential()
   model.add(Input((h, w, 3)))
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 2, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 2, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 2, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 1, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-  model.add(Conv2D(256, 3, 2, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
-
+  model.add(Conv2D(256, 5, 2, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
+  model.add(Conv2D(256, 5, 2, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
+  model.add(Conv2D(256, 5, 2, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
   model.add(Flatten())
-  model.add(Dense(1024, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
   model.add(Dense(1024, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
   model.add(Dense(2 * numberOfPoints, activation='sigmoid'))
 
